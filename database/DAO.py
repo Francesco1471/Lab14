34d77771fdda76874335c27b36f65f076dd525b1
S313850 <francesco.schiavello1406@gmail.com>
2025-06-04 17:29:44 +0200

    enkldqwhiodjiopjop

--- database/DAO.py
+++ database/DAO.py
@@ -34,3 +34,32 @@ class DAO():
         cursor.close()
         return result
 
+    @staticmethod
+    def get_all_edges(store, interval):
+        cnx = DBConnect.get_connection()
+        cursor = cnx.cursor(dictionary=True)
+        result = []
+        query = """select o1.order_id as Ordine1, o2.order_id as Ordine2, (select sum(t.quantity)
+        														from order_items t
+        														where t.order_id = o1.order_id) 	
+        													    + 
+        													    (select sum(t.quantity)
+        														from order_items t
+        														where t.order_id = o2.order_id)	
+        													 as peso
+        from orders o1, orders o2
+        where o2.store_id = %s
+        and o1.store_id = %s
+        and o1.order_id <> o2.order_id 
+        and o1.order_date > o2.order_date 
+        and ABS(datediff(o1.order_date, o2.order_date)) < %s"""
+
+        cursor.execute(query, (store,store,interval))
+        for row in cursor:
+            result.append(Ordine(**row))
+        cnx.close()
+        cursor.close()
+        return result
+
+
+
